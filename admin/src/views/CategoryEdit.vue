aff06ee161f9821d4438a32488a23e128af75d8c
caizihua <1668281469@qq.com>
2021-03-28 23:59:30 +0800

    23.59

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,12 +2,27 @@
   <div class="about">
     <!-- 根据id显示，如果有就是编辑，如果没有就是新建 -->
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
-    <el-form label-width="80px" @submit.native.prevent="save">
+    <el-form label-width="80px">
+      <!-- @submit.native.prevent="save" -->
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <!-- 展示的是名称，真正存的value是id -->
+          <el-option
+            v-for="item in parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" native-type="submit">提交</el-button>
+        <el-button type="primary" @click="save">
+          <!-- native-type="submit" -->
+          提交</el-button
+        >
       </el-form-item>
     </el-form>
   </div>
@@ -21,23 +36,32 @@ export default {
   },
   data() {
     return {
-      model: {},
+      model: {
+        name: "",
+      },
+      //获取分类名的数组，作为上级分类的可选项
+      parents: [],
     };
   },
   methods: {
     async save() {
       //对于新建和编辑，保存的方法不一样，一个是post，一个是put
       //async await将类似同步的写法写成异步
-      let res;
+      // let res;
       if (this.id) {
-        res = await this.$http.put(`categories/${this.id}`, this.model);
+        await this.$http.put(`categories/${this.id}`, this.model);
+        this.$router.push("/categories/list");
       } else {
-        res = await this.$http.post("categories", this.model);
+        if (this.model.name == "") {
+          this.$message({ message: "请输入名称", type: "warning" });
+          return false;
+        } else {
+          await this.$http.post("categories", this.model);
+          this.$router.push("/categories/list");
+        }
       }
-
-      console.log(res);
       //跳转到分类页面
-      this.$router.push("/categories/list");
+
       //提示保存成功
       this.$message({
         type: "success",
@@ -49,10 +73,17 @@ export default {
       const res = await this.$http.get(`categories/${this.id}`);
       this.model = res.data;
     },
+    //获取分类名称，接口就是categories
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
+    },
   },
   created() {
     //前面的条件满足才执行后面的函数
     this.id && this.fetch();
+    //获取父级分类名
+    this.fetchParents();
   },
 };
 </script>
--- server/models/Category.js
+++ server/models/Category.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   //现在数据库中只包含了名字
   name: { type: String },
+  //不是String类型，而是mongodb的objectId类型
+  //ref关联的模型就是Category
+  paren: { type: mongoose.SchemaTypes.ObjectId, ref: "Category" },
 });
 //生成并导出模型
 module.exports = mongoose.model("Category", schema);

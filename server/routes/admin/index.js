88975a515e610ed034adb47ee10f79e675aaf809
caizihua <1668281469@qq.com>
2021-09-14 22:40:38 +0800

    2021/9/14

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -60,7 +60,9 @@ module.exports = (app) => {
   //multer用于处理form-data或者multipart,会添加file对象到req中包含表单上传的文件信息
   //dest属性表示在哪里存储文件,接受单个文件的上传名字为file，因为接口formData中的名字就是file
   const multer = require("multer");
-  const upload = multer({ dest: __dirname + "/../../uploads" });
+  const upload = multer({
+    dest: __dirname + "/../../uploads"
+  });
   app.post(
     "/admin/api/upload",
     authMiddleware(),
@@ -74,23 +76,29 @@ module.exports = (app) => {
   );
   //8token
   app.post("/admin/api/login", async (req, res) => {
-    const { username, password } = req.body;
+    const {
+      username,
+      password
+    } = req.body;
     //根据用户名找用户,将password强制取出
-    const user = await AdminUser.findOne({ username }).select("+password");
+    const user = await AdminUser.findOne({
+      username
+    }).select("+password");
     assert(username !== "", 422, "请输入用户名");
     assert(user, 422, "用户不存在");
     //校验密码,将明文和密文进行比对
     const isValid = require("bcrypt").compareSync(password, user.password);
     assert(isValid, 422, "密码错误");
     //返回token
-    const token = jwt.sign(
-      {
+    const token = jwt.sign({
         id: user._id,
       },
       //只能得到一个参数，获取配置
       app.get("secret")
     );
-    res.send({ token });
+    res.send({
+      token
+    });
   });
   //错误处理
   app.use(async (err, req, res, next) => {
